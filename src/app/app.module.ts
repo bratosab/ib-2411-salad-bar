6c354ef86596e909f7358b684c25a068a0ddbbc9
Abdallah <bratosab@gmail.com>
2024-11-25 15:23:59 +0100

    feat(salad bar): structure of project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { SharedMaterialModule } from './shared-material.module';
+import { SaladModule } from './salad/salad.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,9 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedMaterialModule,
+    SaladModule
   ],
   providers: [
     provideAnimationsAsync()

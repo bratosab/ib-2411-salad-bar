1f4fadf4ce8e91ba648fd7758b2d17fb5c88fa27
Abdallah <bratosab@gmail.com>
2024-11-27 14:04:09 +0100

    feat(computed): used a computed signal to calculate total price

--- src/app/services/salad.service.ts
+++ src/app/services/salad.service.ts
@@ -1,26 +1,30 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, signal } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
 import { Topping } from '../models/topping.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SaladService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getToppings() {
-    return this.http.get<Topping[]>('https://retoolapi.dev/udhTkG/toppings')
+    return this.http.get<Topping[]>('https://retoolapi.dev/udhTkG/toppings');
   }
 
-  private chosenToppingList = signal<Topping[]>([])
+  private chosenToppingList = signal<Topping[]>([]);
+  totalPrice = computed(() =>
+    this.chosenToppingList().reduce((total, topping) => {
+      return total + topping.price;
+    }, 0)
+  );
 
   get chosenToppings() {
-    return this.chosenToppingList.asReadonly()
+    return this.chosenToppingList.asReadonly();
   }
 
   chooseTopping(topping: Topping) {
-    this.chosenToppingList.update((list) => [...list, topping])
+    this.chosenToppingList.update((list) => [...list, topping]);
     // this.chosenToppingList.update((list) => {
     //   const toppings = list.slice()
     //   toppings.push(topping)
@@ -29,7 +33,8 @@ export class SaladService {
   }
 
   removeTopping(id: number) {
-    this.chosenToppingList.update(list => list.filter(topping => topping.id !== id))
+    this.chosenToppingList.update((list) =>
+      list.filter((topping) => topping.id !== id)
+    );
   }
-
 }

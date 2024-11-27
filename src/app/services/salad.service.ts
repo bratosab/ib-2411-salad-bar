0e9ba1c2f776cbe77e986dc7667e85c4691f8e45
Abdallah <bratosab@gmail.com>
2024-11-27 11:19:02 +0100

    feat(salad service): added persist for chosen toppings

--- src/app/services/salad.service.ts
+++ src/app/services/salad.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import { Topping } from '../models/topping.model';
 
 @Injectable({
@@ -12,4 +12,24 @@ export class SaladService {
   getToppings() {
     return this.http.get<Topping[]>('https://retoolapi.dev/udhTkG/toppings')
   }
+
+  private chosenToppingList = signal<Topping[]>([])
+
+  get chosenToppings() {
+    return this.chosenToppingList.asReadonly()
+  }
+
+  chooseTopping(topping: Topping) {
+    this.chosenToppingList.update((list) => [...list, topping])
+    // this.chosenToppingList.update((list) => {
+    //   const toppings = list.slice()
+    //   toppings.push(topping)
+    //   return toppings
+    // })
+  }
+
+  removeTopping(id: number) {
+    this.chosenToppingList.update(list => list.filter(topping => topping.id !== id))
+  }
+
 }

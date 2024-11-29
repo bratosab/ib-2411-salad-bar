55c2c5953db9d4217d907e008ac0c1bcf51e1df0
Abdallah <bratosab@gmail.com>
2024-11-29 16:17:53 +0100

    feat(testing): added service testing

--- src/app/services/salad.service.spec.ts
+++ src/app/services/salad.service.spec.ts
@@ -1,16 +1,43 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SaladService } from './salad.service';
+import { provideHttpClient } from '@angular/common/http';
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from '@angular/common/http/testing';
+import { Topping } from '../models/topping.model';
 
 describe('SaladService', () => {
   let service: SaladService;
+  let httpTesting: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
     service = TestBed.inject(SaladService);
+    httpTesting = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('getToppings() should return a topping list', (done: DoneFn) => {
+    const mockToppings: Topping[] = [{ id: 0, name: 'Carrots', price: 999 }];
+
+    service.getToppings().subscribe((toppings) => {
+      console.log(toppings);
+      expect(toppings).toEqual(mockToppings);
+      done();
+    });
+
+    const intercept = httpTesting.expectOne(
+      'https://retoolapi.dev/udhTkG/toppings'
+    );
+
+    expect(intercept.request.method).toEqual('GET');
+    intercept.flush(mockToppings)
   });
 });
